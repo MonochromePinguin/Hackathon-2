e0dbcd18d9c1504c2355dfcbf2e9a2560f5b3df2
monochromePinguin <jaimaille.com@gmail.com>
2018-06-27 19:48:39 +0200

    Addition of bootstrap 4.1 css and of some basic files

--- casper/src/AppBundle/Controller/DefaultController.php
+++ casper/src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,12 +11,21 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Method({"GET"}
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/testMap", name="map")
+     */
+    public function mapAction()
+    {
+        return $this->render('map.html.twig')
     }
 }
